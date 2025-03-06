7a9ced4992800722622ecc39a19912ecc13910d6
chemnida <chaemin9073@gmail.com>
2025-03-06 15:47:05 +0900

    구조 변경

--- src/main/java/com/shop/cafe/controller/MemberController.java
+++ src/main/java/com/shop/cafe/controller/MemberController.java
@@ -13,26 +13,52 @@ import org.springframework.web.bind.annotation.RestController;
 import com.shop.cafe.dto.Member;
 import com.shop.cafe.service.MemberService;
 
-@RestController
-@CrossOrigin("http://127.0.0.1:5500")
-public class MemberController {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
+@RestController
+//@CrossOrigin("http://127.0.0.1:8080/")
+public class MemberController {
+	
 	@Autowired
 	MemberService memberService;
 	
+	@PostMapping("login")
+	public Map<String, String> login(@RequestBody Member m, HttpServletRequest request) {
+		System.out.println(m);
+		Map<String,String> responseData=new HashMap();
+		try {
+			m=memberService.login(m);
+			if(m!=null) {//login ok
+				System.out.println(m);
+				HttpSession session=request.getSession();
+				System.out.println(session.getId());
+				session.setAttribute("member", m);
+				responseData.put("msg","ok");
+			}else {//login fail
+				responseData.put("msg","다시 로그인 해주세요");
+			}
+		} catch (Exception e) {//login error			
+			e.printStackTrace();
+			responseData.put("msg","다시 로그인 해주세요");
+		}
+		return responseData;
+	}
+	
 	@PostMapping("insertMember")
-	public Map<String,String> insertMember(@RequestBody Member m) {
-		Map<String, String> responseData = new HashMap();
-		
+	public Map<String, String> insertMember(@RequestBody  Member m) {
+		Map<String,String> responseData=new HashMap();
 		try {
 			memberService.insertMember(m);
 			responseData.put("msg","ok");
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 			responseData.put("msg",e.getMessage());
 		}
 		
 		return responseData;
-	
 	}
+	
+
 }
